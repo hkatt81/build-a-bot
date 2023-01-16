ab4fe143780129e6f713d7dd668704c3b5ec2094
Heather Mascall <Heather.Mascall@gmail.com>
2023-01-16 16:32:44 +0800

    Custom directive args notation

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -5,7 +5,7 @@
         <!-- <img :src="selectedPart.src" @click="showPartInfo= !showPartInfo" title="arm"/> -->
         <button @click="selectPreviousPart()" class="prev-selector"></button>
         <button @click="selectNextPart()" class="next-selector"></button>
-        <span class="sale" v-show="selectedPart.onSale">Sale!</span>
+        <span v-pin:position.top.right class="sale" v-show="selectedPart.onSale">Sale!</span>
         <!-- <teleport to="#part-info" v-if="showPartInfo">
             <div>
                 <div>{{ selectedPart.cost }}  {{ selectedPart.title }}
@@ -18,6 +18,7 @@
   </template>
 
 <script>
+import pinDirective from '../shared/pin-directive';
 // import availableParts from '../data/parts';
 
 // const parts = availableParts.heads;
@@ -33,6 +34,9 @@ function getNextValidIndex(index, length) {
 }
 
 export default {
+    directives: {
+        pin: pinDirective,
+    },
     // Inject receives data from parent provide, so can be used
     inject: ['user'],
     props: {
@@ -104,9 +108,6 @@ export default {
     border: 3px solid #aaa;
 }
 .sale {
-    position: absolute;
-    bottom: 5px;
-    right: 5px;
     color: white;
     background-color: red;
     padding: 3px;
---
+++ src/shared/pin-directive.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-param-reassign */
+export default {
+    beforeMount(element, binding) {
+        // console.log('arg:', binding.arg, 'modifiers:', binding.modifiers);
+        if (binding.arg !== 'position') return;
+
+        Object.keys(binding.modifiers).forEach((key) => {
+            element.style[key] = '5px';
+        });
+        element.style.position = 'absolute';
+        // element.style.bottom = '5px';
+        // element.style.right = '5px';
+    },
+};
