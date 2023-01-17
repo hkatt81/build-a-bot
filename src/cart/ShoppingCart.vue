6fd0dda755f057201360364be73c035ce4b178a2
Heather Mascall <Heather.Mascall@gmail.com>
2023-01-17 15:22:25 +0800

    Apply global filter to page

--- src/cart/ShoppingCart.vue
+++ src/cart/ShoppingCart.vue
@@ -18,7 +18,7 @@
               {{robot.head.title}}
             </td>
             <td class="cost">
-              {{currency(robot.cost, '$')}}
+              {{ $filters.currency(robot.cost, '$') }}
             </td>
           </tr>
         </tbody>
@@ -41,7 +41,7 @@
               {{robot.head.title}}
             </td>
             <td class="cost">
-              {{currency(robot.cost, '$')}}
+              {{ $filters.currency(robot.cost, '$') }}
             </td>
           </tr>
         </tbody>
@@ -50,13 +50,8 @@
   </template>
 
 <script>
-// import currencyFilter from '../shared/currency-filter';
-
 export default {
     name: 'Cart',
-    // methods: {
-    //     currency: currencyFilter,
-    // },
     computed: {
         cart() {
             // Accessing data from the store
